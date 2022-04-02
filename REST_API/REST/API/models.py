9e5dc0fb694aff73807434e51d039ff13847ead6
RobertZ225 <77633975+RobertZ225@users.noreply.github.com>
2022-04-02 01:35:32 -0600

    Added first table (no foreign keys yet)

--- REST_API/REST/API/models.py
+++ REST_API/REST/API/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class User (models.Model):
+    UserId = models.AutoField(primary_key=True)
+    FName = models.CharField(max_length=35)
+    LName = models.CharField(max_length=35)
+    Location = models.CharField(max_length=255)
+    DNo = models.IntegerField(null=True)
+    UserUsername = models.CharField(max_length=255)
+
+    class Meta:
+        app_label='API'
+
+    def __str__(self):
+        return self.UserUsername
