89f830304244a177dc73c341e5095ea5343c74e2
RobertZ225 <77633975+RobertZ225@users.noreply.github.com>
2022-04-14 04:52:30 -0600

    Optimized index.html. It has no more async queries

--- JavaScript/queries.js
+++ JavaScript/queries.js
@@ -2,3 +2,37 @@ var jquery = document.createElement('script');
 jquery.src = "http://code.jquery.com/jquery-latest.min.js";
 document.getElementsByTagName("head")[0].appendChild(jquery);
 
+function initUser(username, password)
+{
+    var userBackup;
+    var settings = 
+    {
+        "async": false,
+        "crossDomain": true,
+        "url": "http://127.0.0.1:8000/API/v1/login/" + username + "/" + password,
+        "method": "GET",
+    };
+    $.ajax(settings).done(function (response){
+        localStorage.setItem('useridentity', JSON.stringify(response));
+
+    });
+    console.log(localStorage.getItem("useridentity"));
+
+    return "useridentity";
+}
+
+function initManagerList()
+{
+    var settings = 
+        {
+            "async": false,
+            "crossDomain": true,
+            "url": "http://127.0.0.1:8000/API/v1/managers/",
+            "method": "GET",
+        };
+        $.ajax(settings).done(function (response){
+            localStorage.setItem('managerList', JSON.stringify(response));
+        });
+
+        return "managerList";
+}
